b620587b90c945082e7797b5f49625d4e9282016
gabrieldevreis <gabrielreisdev@gmail.com>
2025-12-01 23:20:25 -0300

    refactor: organizado importações e declarações no AppModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SiderbarComponent } from './pages/siderbar/siderbar.component';
+import { CertificatesComponent } from './pages/certifications/certificates.component';
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ProjectsComponent,
+    SiderbarComponent,
+    SiderbarComponent,
+    CertificatesComponent,
+    TechnologiesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, AppRoutingModule, RouterModule],
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
