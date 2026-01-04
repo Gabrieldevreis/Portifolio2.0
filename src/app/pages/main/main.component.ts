5739af4a2bccc303d37cb91022ebc73f08c88205
gabrieldevreis <gabrielreisdev@gmail.com>
2026-01-04 17:39:27 -0300

    feat: refatora layout e responsividade do MainComponent e NavigationComponent

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,36 +1,9 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css',
 })
-export class MainComponent implements OnInit {
-  isMobile = false;
-  showSidebar = true;
-
-  ngOnInit(): void {
-    this.checkScreen();
-  }
-
-  @HostListener('window:resize')
-  checkScreen() {
-    this.isMobile = window.innerWidth < 916;
-
-    if (!this.isMobile) {
-      this.showSidebar = true;
-    }
-  }
-
-  onNavigate() {
-    if (this.isMobile) {
-      this.showSidebar = false;
-    }
-  }
-
-  openSidebar() {
-    if (this.isMobile) {
-      this.showSidebar = true;
-    }
-  }
+export class MainComponent {
 }
