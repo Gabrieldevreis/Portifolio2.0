bd36630c5ec4090b6c28d7e3f6327f42114cad0c
gabrieldevreis <gabrielreisdev@gmail.com>
2026-01-04 11:52:55 -0300

    feat: atualiza estilos e estrutura do layout, incluindo melhorias na responsividade e na aparência do sidebar e navigation

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrl: './main.component.css'
+  styleUrl: './main.component.css',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  isMobile = false;
+  showSidebar = true;
 
+  ngOnInit(): void {}
+
+  @HostListener('window:resize')
+  checkScreen() {
+    this.isMobile = window.innerWidth < 916;
+
+    if (!this.isMobile) {
+      this.showSidebar = true;
+    }
+  }
+
+  onNavigate() {
+    if (this.isMobile) {
+      this.showSidebar = false;
+    }
+  }
+
+  openSidebar() {
+    if (this.isMobile) {
+      this.showSidebar = true;
+    }
+  }
 }
